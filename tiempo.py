6daf63f1d1c02a70bafdc3f2782fb65f777a9b8e
mel <melanieromero7111@gmail.com>
2024-08-18 22:45:51 -0300

    modulo tiempo

--- tiempo.py
+++ tiempo.py
@@ -2,32 +2,32 @@ import time
 
 class cronometro:
     def __init__(self):
-        self.tiempoInicio = 0
-        self.tiempoTotal = 0
+        self.tiempo_inicio = 0
+        self.tiempo_total = 0
         self.transcurriendo = False
         
     def iniciar(self):
         if not self.transcurriendo:
             # Ajusta el tiempoInicio considerando el tiempo ya transcurrido
-            self.tiempoInicio = time.time() - self.tiempoTotal
+            self.tiempo_inicio = time.time() - self.tiempo_total
             self.transcurriendo = True
             
     def detener(self):
         if self.transcurriendo:
             # Calcula el tiempo total transcurrido hasta ahora
-            self.tiempoTotal = time.time() - self.tiempoInicio
+            self.tiempo_total = time.time() - self.tiempo_inicio
             self.transcurriendo = False
             
     def actualizar_tiempo(self):
         if self.transcurriendo:
-            tiempo_actual = time.time() - self.tiempoInicio
+            tiempo_actual = time.time() - self.tiempo_inicio
             minutos = int(tiempo_actual // 60)
             segundos = int(tiempo_actual % 60)
             milisegundos = int((tiempo_actual * 100) % 100)
             return f"{minutos:02}", f"{segundos:02}", f"{milisegundos:02}"
         else:
             # Si el cronómetro está detenido, devuelve el tiempo total acumulado
-            minutos = int(self.tiempoTotal // 60)
-            segundos = int(self.tiempoTotal % 60)
-            milisegundos = int((self.tiempoTotal * 100) % 100)
+            minutos = int(self.tiempo_total // 60)
+            segundos = int(self.tiempo_total % 60)
+            milisegundos = int((self.tiempo_total * 100) % 100)
             return f"{minutos:02}", f"{segundos:02}", f"{milisegundos:02}"
