5cd27ff286e27239a7cc0c101fab794cfa219454
mel <melanieromero7111@gmail.com>
2024-09-17 16:43:59 -0300

    cambios en mostrar mejores (tiempo segundos)

--- mejores_tiempos.py
+++ mejores_tiempos.py
@@ -4,19 +4,18 @@ from utilidades import dibujar_texto,dibujar_texto_borde
 def convertir_a_segundos(tiempo_str):
     #Convierte un string en formato HH:MM:SS a segundos.
     partes = tiempo_str.split(':')  # Divide la cadena en horas, minutos y segundos
-    horas = int(partes[0])
-    minutos = int(partes[1])
-    segundos = int(partes[2])
-    
+    minutos = int(partes[0])
+    segundos = int(partes[1])
+    milisegundos = int(partes[2])
+    print(f"{minutos},{segundos},{milisegundos}")
     # Convertir todo a segundos
-    total_segundos = horas * 3600 + minutos * 60 + segundos
+    total_segundos = minutos * 60 + segundos  + milisegundos / 100 
     return total_segundos
 
 def extraer_mejores_marcas(ruta_nombres, ruta_tiempos, num_mejores=10):
     #Extrae las mejores marcas desde los archivos y las devuelve ordenadas.
     nombres = archivos_lectores(ruta_nombres)
     tiempos = archivos_lectores(ruta_tiempos)
-    
     # Crear una lista de tuplas (nombre, tiempo en segundos)
     marcas = []
     for nombre, tiempo in zip(nombres, tiempos):
@@ -33,15 +32,13 @@ def extraer_mejores_marcas(ruta_nombres, ruta_tiempos, num_mejores=10):
     return marcas_ordenadas[:num_mejores]
 
 def mostrar_mejores_marcas(pantalla, mejores_marcas):
-    #Muestra las mejores marcas en una ventana de Pygame.
+    global boton_volver
     fuente_title = pygame.font.Font("fonts/pixel_digivolve/Pixel Digivolve.otf", 55)
     fuente = pygame.font.Font("fonts/depixel/DePixelBreit.ttf", 20)
     fondo = pygame.transform.scale(pygame.image.load("img/fondo_puntaje.png").convert_alpha(),(pantalla.get_width(),pantalla.get_height()))
-    global boton_volver
-    img_boton_volver = pygame.transform.scale(pygame.image.load('img/boton.png').convert_alpha(), (220, 85))
-    boton_volver = pygame.Rect(500, 540, 220, 85)
+    boton_volver = pygame.transform.scale(pygame.image.load('img/boton.png').convert_alpha(), (220, 85))
     pantalla.blit(fondo,(0,0))
-    pantalla.blit(img_boton_volver,(500,540))
+    pantalla.blit(boton_volver,(500,540))
     # Título
     dibujar_texto_borde("Mejores marcas",fuente_title,(255,255,255),330,12,pantalla)
     dibujar_texto(pantalla,"Ranking",fuente,(197,57,178),210,130)
@@ -51,12 +48,13 @@ def mostrar_mejores_marcas(pantalla, mejores_marcas):
     #  Mostrar las mejores marcas
     for i, (nombre, tiempo) in enumerate(mejores_marcas):
         # Convertir de nuevo a formato HH:MM:SS para mostrarlo
-        horas = int(tiempo // 3600)
-        minutos = int((tiempo % 3600) // 60)
+        print(f"segundos total {tiempo}")
+        minutos = int(tiempo // 60)
         segundos = int(tiempo % 60)
-        print(f"{horas}, {minutos},{segundos}")
-        tiempo_formateado = f"{horas:02}:{minutos:02}:{segundos:02}"
-        texto = f"{i + 1:<17} {nombre:<20} {tiempo_formateado:<15}"
+        milisegundos = int((tiempo - int(tiempo)) * 1000)
+        print(f"{minutos}, {segundos},{milisegundos}")
+        tiempo_formateado = f"{minutos:02}:{segundos:02}:{milisegundos:02}"
+        texto = f"{i + 1:<16} {nombre:<20} {tiempo_formateado:<15}"
         texto_renderizado = fuente.render(texto, True, (255, 255, 255))
         pantalla.blit(texto_renderizado, (210, 180 + i * 35))
 
@@ -91,7 +89,6 @@ def main():
             
             if event.type ==  pygame.MOUSEBUTTONDOWN:
                 if boton_volver.collidepoint(event.pos):
-                    print("toco boton volver")
                     corriendo = False
                     return "volver"
     pygame.quit()
